f8507c894d31c74a507e5d36c6fbf1e5eaf3a2c8
dancan-sandys <dancan.oruko96@gmail.com>
2020-10-10 01:17:08 +0300

    add the user, locations and category models

--- gallery/settings.py
+++ gallery/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mygallery.apps.MygalleryConfig'
+    
 ]
 
 MIDDLEWARE = [
@@ -62,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },
@@ -75,8 +78,11 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'sandys',
+        'PASSWORD': 'Stanford1*',
+
     }
 }
 
@@ -105,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
--- mygallery/models.py
+++ mygallery/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import datetime as dt   
 
 # Create your models here.
+
+class Location(models.Model):
+    country= models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+class Image(models.Model):
+
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    date = models.DateField(auto_now_add=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE,)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,)
+
+
